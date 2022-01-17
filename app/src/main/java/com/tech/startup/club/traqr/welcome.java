4859b4b6cb7b024ff71254b74f3925dc3d5d1bf3
Derek Li <rejie.li@gmail.com>
2022-01-17 17:27:23 -0500

    changed buttons

--- app/src/main/java/com/tech/startup/club/traqr/welcome.java
+++ app/src/main/java/com/tech/startup/club/traqr/welcome.java
@@ -3,8 +3,11 @@ package com.tech.startup.club.traqr;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
 
 public class welcome extends AppCompatActivity {
+
+    private Button button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
