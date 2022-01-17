e3e5dc44f1394575466d191cdccba391894e2b35
Derek Li <rejie.li@gmail.com>
2022-01-17 17:39:00 -0500

    changed buttons

--- app/src/main/java/com/tech/startup/club/traqr/welcome.java
+++ app/src/main/java/com/tech/startup/club/traqr/welcome.java
@@ -2,16 +2,33 @@ package com.tech.startup.club.traqr;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
+
+import com.tech.startup.club.traqr.ui.login.LoginActivity;
 
 public class welcome extends AppCompatActivity {
 
-    private Button button;
+    private Button login;
+    private Button signIn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_welcome);
+
+        login = (Button) findViewById(R.id.login);
+        login.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+//                Intent intent = new Intent(this, LoginActivity.class);
+//                startActivity(intent);
+            }
+        });
+
     }
+
+
 }
