916e8d4864009d3c30549616e555c855990da3e1
Kevin Wu <kevwuhoo15@gmail.com>
2022-02-27 00:05:54 -0500

    QR generator

---
+++ app/src/main/java/com/tech/startup/club/traqr/QRCode/QRCodeGenerator.java
@@ -0,0 +1,95 @@
+package com.tech.startup.club.traqr.QRCode;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.graphics.Point;
+import android.os.Bundle;
+import android.os.Environment;
+import android.util.Log;
+import android.view.Display;
+import android.view.View;
+import android.view.WindowManager;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+import com.tech.startup.club.traqr.R;
+
+import androidmads.library.qrgenearator.QRGContents;
+import androidmads.library.qrgenearator.QRGEncoder;
+import androidmads.library.qrgenearator.QRGSaver;
+
+public class QRCodeGenerator extends AppCompatActivity {
+    EditText qrvalue;
+    Button generateBtn;
+    ImageView qrImage;
+    private String inputValue;
+    private String savePath = Environment.getExternalStorageDirectory().getPath() + "/QRCode/";
+    private Bitmap bitmap;
+    private QRGEncoder qrgEncoder;
+    private AppCompatActivity activity;
+    public static final String TAG = "ERROR";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_qrcode_generator);
+
+        qrvalue = findViewById(R.id.qrInput);
+        generateBtn = findViewById(R.id.generatebutton);
+        qrImage = findViewById(R.id.qrPlaceHolder);
+
+        generateBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String data = qrvalue.getText().toString();
+                if(data.length() > 0){
+                    WindowManager manager = (WindowManager) getSystemService(WINDOW_SERVICE);
+                    Display display = manager.getDefaultDisplay();
+                    Point point = new Point();
+                    display.getSize(point);
+                    int width = point.x;
+                    int height = point.y;
+                    int smallerDimension = Math.min(width, height);
+                    smallerDimension = smallerDimension * 3 / 4;
+                QRGEncoder qrgEncoder = new QRGEncoder(data, null, QRGContents.Type.TEXT, smallerDimension);
+                    try {
+                        // Getting QR-Code as Bitmap
+                        bitmap = qrgEncoder.getBitmap();
+                        // Setting Bitmap to ImageView
+                    qrImage.setImageBitmap(bitmap);
+                    } catch (Exception e) {
+                        Log.v(TAG, e.toString());
+                    }
+
+                }
+                else{
+                    qrvalue.setError("Error");
+                }
+        }
+    });
+        findViewById(R.id.save_barcode).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (ContextCompat.checkSelfPermission(getApplicationContext(), Manifest.permission.WRITE_EXTERNAL_STORAGE) == PackageManager.PERMISSION_GRANTED) {
+                    try {
+                        boolean save = new QRGSaver().save(savePath, qrvalue.getText().toString().trim(), bitmap, QRGContents.ImageType.IMAGE_JPEG);
+                        String result = save ? "Image Saved" : "Image Not Saved";
+                        Toast.makeText(activity, result, Toast.LENGTH_LONG).show();
+                        qrvalue.setText(null);
+                    } catch (Exception e) {
+                        e.printStackTrace();
+                    }
+                } else {
+                    ActivityCompat.requestPermissions(activity, new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE}, 0);
+                }
+            }
+        });
+}
+}
--- app/src/main/java/com/tech/startup/club/traqr/homepage/Camera.java
+++ app/src/main/java/com/tech/startup/club/traqr/homepage/Camera.java
@@ -2,15 +2,20 @@ package com.tech.startup.club.traqr.homepage;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 import com.google.firebase.auth.FirebaseAuth;
+import com.tech.startup.club.traqr.QRCode.QRCodeGenerator;
 import com.tech.startup.club.traqr.R;
 
 public class Camera extends AppCompatActivity {
 
     private FirebaseAuth mAuth;
+    private Button qrCode;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,6 +27,14 @@ public class Camera extends AppCompatActivity {
         TextView tv1 = (TextView)findViewById(R.id.textView);
 
         tv1.setText(mAuth.getCurrentUser().getUid());
-
+        //temporary qrcode button
+        qrCode = (Button) findViewById(R.id.qrcode);
+        qrCode.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), QRCodeGenerator.class);
+                startActivity(intent);
+            }
+        });
     }
 }
