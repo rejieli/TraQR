ae3e2544fb5737b766fb34cbe332280c58e5debf
calebl1027 <caleb.li1027@gmail.com>
2022-03-11 15:05:18 -0500

    qr scanner

--- app/src/main/java/com/tech/startup/club/traqr/homepage/Camera.java
+++ app/src/main/java/com/tech/startup/club/traqr/homepage/Camera.java
@@ -1,10 +1,12 @@
 package com.tech.startup.club.traqr.homepage;
 
 import androidx.annotation.Nullable;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
 import androidx.core.content.ContextCompat;
 
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
@@ -21,8 +23,11 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.zxing.integration.android.IntentIntegrator;
+import com.google.zxing.integration.android.IntentResult;
 import com.tech.startup.club.traqr.QRCode.QRCodeGenerator;
 import com.tech.startup.club.traqr.R;
 
@@ -45,8 +50,9 @@ public class Camera extends AppCompatActivity {
             ActivityCompat.requestPermissions(Camera.this, new String[]{
                     Manifest.permission.CAMERA
             }, 100);
-
         }
+
+
 
         //temporary qrcode button
         qrCode = (Button) findViewById(R.id.qrcode);
@@ -58,19 +64,41 @@ public class Camera extends AppCompatActivity {
             }
         });
 
-        btOpenCam.setOnClickListener(v -> {
-            Intent open = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
-            startActivityForResult(open, 100);
-        });
-    }
-
-    @Override
-    public void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
-        super.onActivityResult(requestCode, resultCode, data);
-        if(requestCode ==100) {
-            Bitmap photo = (Bitmap) data.getExtras().get("data");
-            imageview.setImageBitmap(photo);
+        btOpenCam.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                IntentIntegrator intentIntegrator = new IntentIntegrator(Camera.this
+                );
+                intentIntegrator.setBeepEnabled(true);
+                intentIntegrator.setOrientationLocked(true);
+                intentIntegrator.setCaptureActivity(Capture.class);
+                intentIntegrator.initiateScan();
+                }
+            });
         }
 
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        IntentResult intRes = IntentIntegrator.parseActivityResult(requestCode,resultCode,data
+        );
+
+        if(intRes.getContents() != null){
+            AlertDialog.Builder build = new AlertDialog.Builder(
+                    Camera.this
+            );
+            build.setTitle("Result");
+            build.setMessage(intRes.getContents());
+            build.setPositiveButton("OK", new DialogInterface.OnClickListener() {
+                @Override
+                public void onClick(DialogInterface dialog, int which) {
+                    dialog.dismiss();
+                }
+            });
+            build.show();
+        }else{
+            Toast.makeText(getApplicationContext(), "No input",Toast.LENGTH_SHORT).show();
+        }
     }
 }
+
